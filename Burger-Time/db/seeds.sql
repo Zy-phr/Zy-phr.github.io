d0d0e99bfb3c5afbf19cdbfcfc8f256634d767c0
Zy-phr <richard.sanchez79@yahoo.com>
2018-09-15 14:04:10 -0500

    Modifid

--- Burger-Time/db/seeds.sql
+++
@@ -1,8 +0,0 @@
--- Queries --
-
-USE burgers_db;
-INSERT INTO burgers (burger_name, devoured, date) 
-VALUES 
-	('The Avocado Burger', false, CURRENT_TIMESTAMP),
-	('The Double Bacon Burger', false, CURRENT_TIMESTAMP),
-	('The Avocado Bacon Burger', false, CURRENT_TIMESTAMP);
